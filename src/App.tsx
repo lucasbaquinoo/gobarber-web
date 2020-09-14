dc3ff62d66adb8901989551e90041d5bfeb1616e
lucasbaquino <lucasbaquino@icloud.com>
2020-09-14 18:12:55 -0300

    Add Sign Up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import SignIn from './Pages/SignIn';
+// import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
-)
+);
 
 export default App;
